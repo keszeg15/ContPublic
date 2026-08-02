c7e75b6afc5703a8b577e544345d03038c909e22
keszeg15 <keszeg15@gmail.com>
2026-08-02 14:14:40 +0200

    sux34

--- quartz/components/PhotoSwipe.tsx
+++ quartz/components/PhotoSwipe.tsx
@@ -1,19 +1,11 @@
 import { QuartzComponent, QuartzComponentConstructor } from "./types"
 
 const PhotoSwipe: QuartzComponent = () => {
-  return null
+  return <div id="photoswipe-test">PHOTOSWIPE HERE</div>
 }
-
 
 PhotoSwipe.afterDOMLoaded = `
   console.log("PHOTOSWIPE COMPONENT LOADED")
-
-  document.querySelectorAll("article img").forEach((img) => {
-    console.log("FOUND IMAGE", img)
-  })
-
-  import("./scripts/photoswipe.inline.ts")
 `
-
 
 export default (() => PhotoSwipe) satisfies QuartzComponentConstructor
